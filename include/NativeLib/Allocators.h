f3abb09d7468b75c5e02c8befbbc03aeaf86d8c7
Nicco <funkynicco@gmail.com>
2019-11-23 20:46:43 +0100

    Added memory class

--- include/NativeLib/Allocators.h
+++ include/NativeLib/Allocators.h
@@ -7,6 +7,49 @@ namespace nl
 {
     namespace memory
     {
+        class Memory
+        {
+        private:
+            struct MemoryDataInfo
+            {
+                LONG References;
+                size_t Size;
+            };
+
+            static MemoryDataInfo s_zeroSizeData;
+            static const void* const s_zeroSizeDataPointer;
+
+        public:
+            ~Memory();
+
+            Memory(const Memory& memory) noexcept;
+            Memory(Memory&& memory) noexcept;
+            Memory& operator =(const Memory& memory) noexcept;
+            Memory& operator =(Memory&& memory) noexcept;
+
+            static Memory Allocate(size_t size);
+
+            operator bool() const { return m_lp != nullptr; }
+            operator void* () { return m_lp; }
+            operator const void*() const { return m_lp; }
+
+            size_t GetSize() const;
+
+            void* Get() { return m_lp; }
+            const void* Get() const { return m_lp; }
+
+            template <typename T>
+            T* Get() { return static_cast<T*>(m_lp); }
+
+            template <typename T>
+            const T* Get() const { return static_cast<T*>(m_lp); }
+
+        private:
+            void* m_lp;
+            
+            Memory(void* lp) noexcept;
+        };
+
         typedef void*(*AllocateCallback)(size_t size);
         typedef void*(*ReallocateCallback)(void* ptr, size_t size);
         typedef void(*FreeCallback)(void* ptr);
--- src/Allocators.cpp
+++ src/Allocators.cpp
@@ -6,6 +6,91 @@ namespace nl
 {
     namespace memory
     {
+        // memory class
+
+        Memory::MemoryDataInfo Memory::s_zeroSizeData = { 1, 0 };
+        const void* const Memory::s_zeroSizeDataPointer = reinterpret_cast<const void*>(&Memory::s_zeroSizeData + 1);
+
+        Memory::Memory(void* lp) noexcept :
+            m_lp(lp)
+        {
+        }
+
+        Memory::~Memory()
+        {
+            auto info = reinterpret_cast<MemoryDataInfo*>(m_lp) - 1;
+            if (InterlockedDecrement(&info->References) == 0)
+                nl::memory::Free(info);
+        }
+
+        Memory::Memory(const Memory& memory) noexcept
+        {
+            auto info = reinterpret_cast<MemoryDataInfo*>(memory.m_lp) - 1;
+            InterlockedIncrement(&info->References);
+
+            m_lp = memory.m_lp;
+        }
+
+        Memory::Memory(Memory&& memory) noexcept
+        {
+            m_lp = memory.m_lp;
+
+            InterlockedIncrement(&s_zeroSizeData.References);
+            memory.m_lp = &s_zeroSizeData;
+        }
+
+        Memory& Memory::operator =(const Memory& memory) noexcept
+        {
+            auto info = reinterpret_cast<MemoryDataInfo*>(m_lp) - 1;
+            if (InterlockedDecrement(&info->References) == 0)
+                nl::memory::Free(info);
+
+            info = reinterpret_cast<MemoryDataInfo*>(memory.m_lp) - 1;
+            InterlockedIncrement(&info->References);
+
+            m_lp = memory.m_lp;
+            return *this;
+        }
+
+        Memory& Memory::operator =(Memory&& memory) noexcept
+        {
+            auto info = reinterpret_cast<MemoryDataInfo*>(m_lp) - 1;
+            if (InterlockedDecrement(&info->References) == 0)
+                nl::memory::Free(info);
+
+            m_lp = memory.m_lp;
+
+            InterlockedIncrement(&s_zeroSizeData.References);
+            memory.m_lp = &s_zeroSizeData;
+            return *this;
+        }
+
+        size_t Memory::GetSize() const
+        {
+            auto info = reinterpret_cast<MemoryDataInfo*>(m_lp) - 1;
+            return info->Size;
+        }
+
+        Memory Memory::Allocate(size_t size)
+        {
+            if (size == 0)
+            {
+                nl_assert_if_debug(size != 0);
+
+                InterlockedIncrement(&s_zeroSizeData.References);
+                return Memory(&s_zeroSizeData);
+            }
+
+            auto info = reinterpret_cast<MemoryDataInfo*>(nl::memory::Allocate(sizeof(MemoryDataInfo) + size));
+            auto lp = reinterpret_cast<void*>(info + 1);
+
+            info->Size = size;
+            info->References = 1;
+            return Memory(lp);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         static AllocateCallback g_allocate = nullptr;
         static ReallocateCallback g_reallocate = nullptr;
         static FreeCallback g_free = nullptr;
@@ -31,11 +116,14 @@ namespace nl
 
         void* Allocate(size_t size)
         {
+            nl_assert_if_debug(size != 0);
             return g_allocate(size);
         }
 
         void* AllocateThrow(size_t size)
         {
+            nl_assert_if_debug(size != 0);
+
             auto ptr = g_allocate(size);;
             if (!ptr)
                 throw BadAllocationException();
@@ -45,11 +133,13 @@ namespace nl
 
         void* Reallocate(void* ptr, size_t size)
         {
+            nl_assert_if_debug(ptr != nullptr);
             return g_reallocate(ptr, size);
         }
 
         void Free(void* ptr)
         {
+            nl_assert_if_debug(ptr != nullptr);
             g_free(ptr);
         }
     }
